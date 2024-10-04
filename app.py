23086db09aee08df6dd3a536d8fffa2582ea37f6
Stux <stux@stux.com>
2024-10-04 21:39:03 +0200

    added first app routes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, render_template
 import sqlite3
 
+# database class, will maybe turn into a module
 class database():
     def __init__(self) -> None:
         self.connection = sqlite3.connect('videos.db')
@@ -33,24 +34,64 @@ class database():
         delete_query = '''
             DELETE FROM videos WHERE id = ?;
         '''
-        self.cursor.execute(delete_query, id) 
+        self.cursor.execute(delete_query, (id,)) 
         self.connection.commit()
 
+    def get_videos(self,):
+        select_query = '''
+            SELECT * FROM videos
+        '''
+        self.cursor.execute(select_query)
+        return self.cursor.fetchall()
 
-
-
+    def get_search(self, term):
+        select_query = '''
+            SELECT * FROM videos
+        '''
+        self.cursor.execute(select_query)
+        videos = self.cursor.fetchall()
+        videos_to_show = []
+        for video in videos:
+            if term in video:
+                videos_to_show.append(video) 
+    
+    def get_video(self, id):
+        select_query = '''
+            SELECT * FROM videos WHERE id ?;
+        '''
+        self.cursor.execute(select_query, (id,))
+        return self.cursor.fetchall()[0]
 
 app = Flask(__name__)
 
 
-def main():
+
+@app.route("/")
+def feed():
+    videos = db.get_videos()
+    
+    return render_template("feed.html", content=videos)
+
+@app.route("/search/<term>")
+def search(term):
+    videos = db.get_search(term)
+
+@app.route("/watch/<id>")
+def watch(id):
+    video = db.get_video(id)
+    
+        
+    
+    return render_template("watch", video=video)
+
+
+
+
+
+
+if __name__=="__main__".
     db = database()
     db.init_db()
     
     
     app.run(host="0.0.0.0", port=80)
-
-
-if __name__=="__main__".
-    main()
-

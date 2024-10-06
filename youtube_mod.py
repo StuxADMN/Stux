4c3352cfc875c3e9d71253a7aa8af49c30a450f7
Stux <stux@stux.com>
2024-10-06 20:14:47 +0200

    updated youtube download module

--- youtube_mod.py
+++ youtube_mod.py
@@ -5,19 +5,9 @@ class YouTubeDownloader:
         self.url = url
         self.yt = YouTube(url)
     
-    def download(self, quality='medium'):
+    def download(self, quality='1080p'):
         stream = None
-        
-        if quality == 'low':
-            stream = self.yt.streams.filter(file_extension='mp4', res="360p").first()
-        elif quality == 'medium':
-            stream = self.yt.streams.filter(file_extension='mp4', res="480p").first()
-        elif quality == 'high':
-            stream = self.yt.streams.filter(file_extension='mp4', res="720p").first()
-        elif quality == 'ultra high':
-            stream = self.yt.streams.filter(file_extension='mp4', res="1080p").first()
-        else:
-            raise ValueError("Quality has to be 'low', 'medium', 'high' or 'ultra high'")
-        
+        stream = self.yt.streams.filter(file_extension='mp4', res=quality).first()
+
         if stream:
             stream.download(output_path="static/content/")
